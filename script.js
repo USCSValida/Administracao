4c5ad840455ee6bc295d6d5a996c69cb6111c46f
USCSValida <validavitor@outlook.com>
2026-01-05 13:01:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-fetch("https://api.github.com/rate_limit")
-.then(r=>{document.getElementById("hora").textContent=new Date(r.headers.get("date")).toLocaleString("pt-BR")})
-.catch(()=>{document.getElementById("hora").textContent=new Date().toLocaleString("pt-BR")})
+fetch("https://api.github.com")
+  .then(res => {
+    const serverDate = res.headers.get("date");
+    const d = new Date(serverDate);
+    document.getElementById("hora").textContent =
+      d.toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+  })
+  .catch(() => {
+    document.getElementById("hora").textContent =
+      new Date().toLocaleString("pt-BR");
+  });
